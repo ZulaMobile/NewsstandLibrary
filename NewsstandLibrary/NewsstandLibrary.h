2040c6b232b45816d965219826762859f1bd199c
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-02-05 18:19:52 +0200

    initial commit

---
+++ NewsstandLibrary/NewsstandLibrary.h
@@ -0,0 +1,15 @@
+//
+//  NewsstandLibrary.h
+//  NewsstandLibrary
+//
+//  Created by Suleyman Melikoglu on 05/02/14.
+//  Copyright (c) 2014 ZulaMobile. All rights reserved.
+//
+
+#import <ZulaLibrary/ZulaLibrary.h>
+
+// view controllers
+#import "SMBookStoreViewController.h"
+
+// models
+#import "SMBookStore.h"
