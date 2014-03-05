1ee1d365a4941f02b574ff74c3c501d667b3f077
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-03-05 16:48:41 +0200

    initial structure change

---
+++ NewsstandLibrary/NewsstandLibrary/SMAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  SMAppDelegate.m
+//  NewsstandLibrary
+//
+//  Created by Suleyman Melikoglu on 05/03/14.
+//  Copyright (c) 2014 ZulaMobile. All rights reserved.
+//
+
+#import "SMAppDelegate.h"
+
+@implementation SMAppDelegate
+
+@end
---
+++ NewsstandLibrary/NewsstandLibrary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NewsstandLibrary
+//
+//  Created by Suleyman Melikoglu on 05/03/14.
+//  Copyright (c) 2014 ZulaMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
